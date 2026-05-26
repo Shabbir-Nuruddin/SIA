bf620a2185eab0d22c6593a9551bc5afd77280f3
avainateya <avainateya@gmail.com>
2026-05-26 14:56:32 +0530

    Update chunkLoader.ts

--- src/lib/rag/chunkLoader.ts
+++ src/lib/rag/chunkLoader.ts
@@ -1,7 +1,6 @@
 // src/lib/rag/chunkLoader.ts
 
-import data from "@/lib/data/physics/unit1_complete.json";
-
+import data from "@/lib/data/physics-edexcel/unit1_complete.json";
 export function loadChunks() {
   return data.chunks;
 }
